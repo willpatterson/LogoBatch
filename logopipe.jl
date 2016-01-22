eed05c8e5eb0f565cf0057d73dfb2582145ff1db
William Patterson <wpatt2@pdx.edu>
2016-01-21 19:20:19 -0800

    Finnshed most of the functions

--- logopipe.jl
+++ logopipe.jl
@@ -1,6 +1,7 @@
 using DataFrames
 using ClusterManagers
 using ArgParse
+using YAML
 
 function parse_commandline()
     settings = ArgParseSettings()
@@ -14,49 +15,82 @@ function parse_commandline()
     return parse_args(settings)
 end
 
-function parse_csv_to_command(csv_file)
+function parse_csv_to_commands(csv_file, outpath="./out/")
     data = readtable(csv_file)
     commands = []
     col_names = names(data)
     for row in eachrow(data)
-        command = "netlogo-headless.sh" #Working right now but could use retooling to make this work for any executable
-        for (i, name) in enumerate(col_names)
-            command = join([command, row[i]], *(replace(string(name), "_", " -"), " "))
+        command = row[1]
+        for (i, name) in enumerate(col_names[2:end])
+            command = join([command, row[i+1]], *(replace(string(name), "_", " -"), " "))
+            command = replace(command, "OUT", outpath)
         end
         push!(commands, command)
     end
     return commands
 end
+
+function unpack_directory(model_directory, dir_name)
+    output_dir = *(dir_name, "_out/") 
+    output_path = join([model_directory, "out", output_dir], "/")
+    mkpath(output_path)
     
-args = parse_commandline()
-println(args)
+    dir_path = join([model_directory, "in", dir_name], "/")
+    dir_listing = readdir(dir_path)
 
-in_dir = join([args["model_directory"], "in"], "/")
-bsearch_dir = join([in_dir, "bsearch"], "/")
-logo_dir = join([in_dir, "nlogo"], "/")
-
-bfiles = readdir(bsearch_dir)
-lfiles = readdir(logo_dir)
-
-for file in bfiles
-    full_path = join([bsearch_dir, file], "/")
-    println(parse_bsearch_csv(full_path))
+    commands = []
+    for item in dir_listing
+        if !startswith(item, ".") && endswith(item, ".csv") && isfile(join([dir_path, item], "/"))
+            csv_file_path = join([dir_path, item], "/")
+            commands = vcat(parse_csv_to_commands(csv_file_path, output_path), commands)
+        end
+    end
+    return commands
 end
-    
-println(bfiles)
-
-#Slurm scheduling
-job_num = 2
-node_num = 1
-
-#=
-addprocs(SlurmManager(job_num), nodes=node_num)
-
-@parallel for i in workers()
-    run(`hostname`)
+        
+function run_job(command)
+    run(`$command`)
 end
 
-for i in workers()
-    rmprocs(i)
+function schedule_jobs(commands, output_dir)
+    job_num = length(commands)
+    node_num = 1
+
+    #slurm_out = join([output_dir, "slurm_output"], "/")
+    #mkdir(slurm_out)
+
+    addprocs(SlurmManager(job_num), nodes=node_num)
+
+    pmap(run_job, commands)
+    #=
+    @parallel for i in workers()
+        run()
+    end
+
+    for i in workers()
+        rmprocs(i)
+    end
+    =#
 end
-=#
+
+function main()    
+    args = parse_commandline()
+
+    in_dir = join([args["model_directory"], "in"], "/")
+    out_dir = join([args["model_directory"], "out"], "/")
+
+    commands = []
+    in_listing = readdir(in_dir)
+    for dir in in_listing
+        if !startswith(dir, ".") && isdir(join([in_dir, dir], "/"))
+            commands = vcat(unpack_directory(args["model_directory"], dir), commands)
+        end
+    end
+    for command in commands
+        println(command)
+    end
+    schedule_jobs(commands, out_dir)
+end
+
+main()
+
