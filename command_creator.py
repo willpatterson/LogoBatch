0eaa1f94d07bf22b42ad9955a0bc388fc6feb2a5
William Patterson <wpatt2@hydra.rc.pdx.edu>
2016-02-04 18:36:06 -0800

    now creates different dirs for different job runs

--- command_creator.py
+++ command_creator.py
@@ -1,72 +1,91 @@
 import os
 import yaml
+import argparse
+import datetime
 
-#executable = "/share/apps/user/stow/netlogo-5.2.1/netlogo-headless.sh"
-#executable = "/share/apps/user/stow/netlogo-5.2.1/behaviorsearch/behaviorsearch_headless.sh"
-#model_name = "BUPwithMUASimple1.nlogo"
-#csv_path = os.path.join(model_path, "in", "OneWaySA77.csv")
-#csv_path = os.path.join(model_path, "in", "bsearchSA.csv")
+def create_commands(model_path):
+    in_path = os.path.join(model_path, "in")
 
-model_path = "/home/wpatt2/ARC/DevBench/LogoPipe/Models/SuperSimple1"
-in_path = os.path.join(model_path, "in")
+    in_files = os.listdir(in_path)
+    in_files = [os.path.join(in_path, infile) for infile in in_files]
 
-in_files = os.listdir(in_path)
-in_files = [os.path.join(in_path, infile) for infile in in_files]
+    yaml_files = [infile for infile in in_files if infile.endswith(".yml")]
 
-yaml_files = [infile for infile in in_files if infile.endswith(".yml")]
+    with open(yaml_files[0], 'r') as yfile:
+        ydata = yaml.load(yfile)
 
-with open(yaml_files[0], 'r') as yfile:
-    ydata = yaml.load(yfile)
-
-
-command_data = []
-for analysis in ydata:
-    command_data.append(analysis["analysis"])
+    command_data = []
+    for analysis in ydata:
+        command_data.append(analysis["analysis"])
 
 
-for analysis in command_data:
-    unique_path = os.path.join(in_path, analysis["unique"])
-    commands = []
-    with open(unique_path, "r") as uni:
-        for count, line in enumerate(uni):
-            if count != 0:
-                unique = line.split(",")[0]
-                commands.append(analysis["command"].format(exe=analysis["exe"],
-                                                           unique=unique,
-                                                           mod=model_path))
-    analysis["commands"] = commands
+    for analysis in command_data:
+        unique_path = os.path.join(in_path, analysis["unique"])
+        commands = []
+        with open(unique_path, "r") as uni:
+            for count, line in enumerate(uni):
+                if count != 0:
+                    unique = line.split(",")[0]
+                    commands.append(analysis["command"].format(exe=analysis["exe"],
+                                                               unique=unique,
+                                                               mod=model_path))
+        analysis["commands"] = commands
 
-print(command_data)
+    return command_data
+
+def create_job_files(command_data, model_path, ntasks):
+    template = ""
+    with open("btemplate.sh", "r") as btemplate:
+        template = btemplate.readlines()
+
+    job_dir = os.path.join(model_path, "in", "jobs", str(datetime.datetime.now().time()))
+    try:
+        os.mkdir(job_dir)
+    except:
+        pass
+
+    file_count = 0
+    for analysis in command_data:
+        job_name = ""
+        for count, com in enumerate(analysis["commands"]):
+            if count % ntasks == 0:
+                file_count += 1
+                job_name = "{name}-job-{count}".format(name=analysis["name"], count=file_count)
+                with open(os.path.join(job_dir, ".".join([job_name, "sh"])), 'w') as bfile:
+                    for line in template:
+                        bfile.write(line)
+                    bfile.write("#SBATCH -o {mod}/out/slurm/{job_name}.out\n".format(mod=model_path, job_name=job_name))
+                    bfile.write("#SBATCH -J {job_name}\n".format(job_name=analysis["name"]))
+            with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'a') as bfile:
+                bfile.write(com + '\n')
+
+    return job_dir
+
+def schedule_jobs(model_path, job_dir):
+    jobs = os.listdir(job_dir)
+    jobs = [os.path.join(job_dir, x) for x in jobs]
+
+    try:
+        os.makedirs(os.path.join(model_path, "out", "slurm"))
+    except FileExistsError:
+        pass
+
+    for job in jobs:
+        os.system("sbatch {}".format(job))
 
 
-template = ""
-with open("btemplate.sh", "r") as btemplate:
-    template = btemplate.readlines()
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("model_path", help="Directory containing models")
+    parser.add_argument("-n", "--ntasks", type=int, default=1, help="Number of tasks per job")
 
-job_dir = "./jobs"
-try:
-    os.mkdir(job_dir)
-except:
-    pass
+    return parser.parse_args()
 
-file_count = 0
-for analysis in command_data:
-    for count, com in enumerate(analysis["commands"]):
-        if count % 1 == 0:
-            file_count += 1
-            with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'w') as bfile:
-                for line in template:
-                    bfile.write(line)
-        with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'a') as bfile:
-            bfile.write(com + '\n')
+def main():
+    args = get_args()
+    command_data = create_commands(args.model_path)
+    job_dir = create_job_files(command_data, args.model_path, args.ntasks)
+    schedule_jobs(args.model_path, job_dir)
 
-
-jobs = os.listdir(job_dir)
-jobs = [os.path.join("./jobs", x) for x in jobs]
-
-try:
-    os.mkdir(os.path.join(model_path, "out"))
-except:
-    pass
-for job in jobs:
-    os.system("sbatch {}".format(job))
+if __name__ == "__main__":
+    main()
