354418916d01a9ca73893a5a3a9954d9f80ff690
William Patterson <wpatt2@hydra.rc.pdx.edu>
2016-02-04 17:23:58 -0800

    Implemented the yaml file system

--- command_creator.py
+++ command_creator.py
@@ -1,42 +1,47 @@
 import os
+import yaml
 
 #executable = "/share/apps/user/stow/netlogo-5.2.1/netlogo-headless.sh"
-executable = "/share/apps/user/stow/netlogo-5.2.1/behaviorsearch/behaviorsearch_headless.sh"
-model_path = "/home/wpatt2/ARC/DevBench/LogoPipe/Models/SuperSimple1"
-model_name = "BUPwithMUASimple1.nlogo"
-
+#executable = "/share/apps/user/stow/netlogo-5.2.1/behaviorsearch/behaviorsearch_headless.sh"
+#model_name = "BUPwithMUASimple1.nlogo"
 #csv_path = os.path.join(model_path, "in", "OneWaySA77.csv")
-csv_path = os.path.join(model_path, "in", "bsearchSA.csv")
+#csv_path = os.path.join(model_path, "in", "bsearchSA.csv")
 
-commands = []
-ncommand = "{exe} --model {mod}BUPwithMUASimple1.nlogo --experiment {exp} --table {mod}/out/{exp}.out --threads 5"
-bcommand = "{exe} -n 3 -p {mod}/{prot} -o {mod}/out/{prot}.out --threads 5"
-"""
-with open(csv_path, "r") as csv:
-    for count, line in enumerate(csv):
-        if count != 0:
-            experiment = line.split(",")[1]
-            commands.append(command.format(exe=executable,
-                                           exp=experiment,
-                                           mod=model_path))
-"""
+model_path = "/home/wpatt2/ARC/DevBench/LogoPipe/Models/SuperSimple1"
+in_path = os.path.join(model_path, "in")
 
-with open(csv_path, "r") as csv:
-    for count, line in enumerate(csv):
-        if count != 0:
-            protocol = line.split(",")[2]
-            commands.append(bcommand.format(exe=executable,
-                                            prot=protocol,
-                                            mod=model_path))
+in_files = os.listdir(in_path)
+in_files = [os.path.join(in_path, infile) for infile in in_files]
+
+yaml_files = [infile for infile in in_files if infile.endswith(".yml")]
+
+with open(yaml_files[0], 'r') as yfile:
+    ydata = yaml.load(yfile)
 
 
-for com in commands:
-    print(com)
+command_data = []
+for analysis in ydata:
+    command_data.append(analysis["analysis"])
+
+
+for analysis in command_data:
+    unique_path = os.path.join(in_path, analysis["unique"])
+    commands = []
+    with open(unique_path, "r") as uni:
+        for count, line in enumerate(uni):
+            if count != 0:
+                unique = line.split(",")[0]
+                commands.append(analysis["command"].format(exe=analysis["exe"],
+                                                           unique=unique,
+                                                           mod=model_path))
+    analysis["commands"] = commands
+
+print(command_data)
+
 
 template = ""
 with open("btemplate.sh", "r") as btemplate:
     template = btemplate.readlines()
-
 
 job_dir = "./jobs"
 try:
@@ -45,14 +50,16 @@ except:
     pass
 
 file_count = 0
-for count, com in enumerate(commands):
-    if count % 3 == 0:
-        file_count += 1
-        with open(os.path.join(job_dir, "job-{}.sh".format(file_count)), 'w') as bfile:
-            for line in template:
-                bfile.write(line)
-    with open(os.path.join(job_dir, "job-{}.sh".format(file_count)), 'a') as bfile:
-        bfile.write(com + '\n')
+for analysis in command_data:
+    for count, com in enumerate(analysis["commands"]):
+        if count % 1 == 0:
+            file_count += 1
+            with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'w') as bfile:
+                for line in template:
+                    bfile.write(line)
+        with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'a') as bfile:
+            bfile.write(com + '\n')
+
 
 jobs = os.listdir(job_dir)
 jobs = [os.path.join("./jobs", x) for x in jobs]
