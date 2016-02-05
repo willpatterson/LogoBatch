da53801b114d13932dd22dbeaf678d54e81ff90b
William Patterson <wpatt2@pdx.edu>
2016-02-05 15:21:34 -0800

    Added cpu flag and enable flag support for yaml input files

--- command_creator.py
+++ command_creator.py
@@ -16,7 +16,8 @@ def create_commands(model_path):
 
     command_data = []
     for analysis in ydata:
-        command_data.append(analysis["analysis"])
+        if analysis["analysis"]["enabled"] is True:
+            command_data.append(analysis["analysis"])
 
 
     for analysis in command_data:
@@ -33,40 +34,45 @@ def create_commands(model_path):
 
     return command_data
 
-def create_job_files(command_data, model_path, ntasks):
+def create_job_files(command_data, model_path, ntasks, run_name):
+    """Creats sbatch files and puts them in their own folder"""
     template = ""
     with open("btemplate.sh", "r") as btemplate:
         template = btemplate.readlines()
 
-    job_dir = os.path.join(model_path, "in", "jobs", str(datetime.datetime.now().time()))
-    try:
-        os.mkdir(job_dir)
-    except:
-        pass
+    job_dir = os.path.join(model_path, "in/jobs", run_name)
 
-    file_count = 0
     for analysis in command_data:
-        job_name = ""
+        file_count = 0
         for count, com in enumerate(analysis["commands"]):
             if count % ntasks == 0:
                 file_count += 1
                 job_name = "{name}-job-{count}".format(name=analysis["name"], count=file_count)
+
                 with open(os.path.join(job_dir, ".".join([job_name, "sh"])), 'w') as bfile:
                     for line in template:
                         bfile.write(line)
-                    bfile.write("#SBATCH -o {mod}/out/slurm/{job_name}.out\n".format(mod=model_path, job_name=job_name))
-                    bfile.write("#SBATCH -J {job_name}\n".format(job_name=analysis["name"]))
-            with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"], file_count)), 'a') as bfile:
+
+                    bfile.write("#SBATCH -J {analysis}\n".format(analysis=analysis["name"]))
+                    bfile.write("#SBATCH --cpus-per-task={cpus}\n".format(cpus=analysis["cpus"]))
+                    bfile.write("#SBATCH -o {mod}/out/slurm/{run_name}/{job_name}.out\n".format(mod=model_path,
+                                                                                                run_name=run_name,
+                                                                                                job_name=job_name))
+
+            with open(os.path.join(job_dir, "{}-job-{}.sh".format(analysis["name"],
+                                                                  file_count)), 'a') as bfile:
                 bfile.write(com + '\n')
 
-    return job_dir
+    return run_name
 
-def schedule_jobs(model_path, job_dir):
+def schedule_jobs(model_path, run_name):
+    """Schedules all of the batch files created in slurm"""
+    job_dir os.path.join(model_path, "in/jobs", run_name)
     jobs = os.listdir(job_dir)
     jobs = [os.path.join(job_dir, x) for x in jobs]
 
     try:
-        os.makedirs(os.path.join(model_path, "out", "slurm"))
+        os.makedirs(os.path.join(model_path, "out/slurm", run_name))
     except FileExistsError:
         pass
 
@@ -75,17 +81,19 @@ def schedule_jobs(model_path, job_dir):
 
 
 def get_args():
+    """Get arguments"""
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("model_path", help="Directory containing models")
-    parser.add_argument("-n", "--ntasks", type=int, default=1, help="Number of tasks per job")
+    parser.add_argument("-m", "--model_path", type=str, default=os.getcwd(), help="Directory containing models")
+    parser.add_argument("-n", "--ntasks", type=int, default=1, help="Number of tasks per batch")
+    parser.add_argument("-r", "--run_name", type=str, default=str(datetime.datetime.now().time()).replace(":", "-"), help="Name of the batch run")
 
     return parser.parse_args()
 
 def main():
     args = get_args()
     command_data = create_commands(args.model_path)
-    job_dir = create_job_files(command_data, args.model_path, args.ntasks)
-    schedule_jobs(args.model_path, job_dir)
+    run_name = create_job_files(command_data, args.model_path, args.ntasks, args.run_name)
+    schedule_jobs(args.model_path, run_name)
 
 if __name__ == "__main__":
     main()
