f40b807ba21dac8ea3e79fd5549652066ac48bdf
William Patterson <wpatt2@pdx.edu>
2016-02-11 15:38:38 -0800

    Removed old files

--- sched_runner.py
+++
@@ -1,8 +0,0 @@
-import os
-
-batches = os.listdir("./batch_scripts")
-batches = [os.path.join("./batch_scripts", x) for x in batches]
-print(batches)
-
-for b in batches:
-    os.system("sbatch {}".format(b))
