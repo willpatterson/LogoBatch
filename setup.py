5b23ff510303cdf6e029c5635e5f3eea0bf88b5b
William Patterson <wpatt2@pdx.edu>
2016-02-18 13:43:23 -0800

    Added the setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="logopipe",
+      version="0.0.1",
+      description="Creates, schedules, and pipes output for jobs in slurm",
+      license="MIT",
+      author="William Patterson",
+      packages=find_packages(),
+      package_data={'logopipe': ['*.yml', '*.txt', '*.sh'],
+      install_requires=["Pyyaml"],
+      long_description="long_description",
+      entry_points={"console_scripts": ["logopipe=logopipe.logopipe:main",]})
