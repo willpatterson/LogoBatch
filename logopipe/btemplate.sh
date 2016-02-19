a591c6342b607f479988771724fdfe7d2886d1ac
William Patterson <wpatt2@pdx.edu>
2016-02-18 16:37:19 -0800

    Commiting all changes before pushing to oo_switch and moving dev env

--- logopipe/btemplate.sh
+++ logopipe/btemplate.sh
@@ -5,11 +5,11 @@
 ##
 
 ##DISABLED:
-##SBATCH --nodes 1
 ##SBATCH --cpus-per-task=8
 
 ##ENABLED:
-#SBATCH --time=30:00:00
+#SBATCH --nodes 1
+#SBATCH --time=144:00:00
 #SBATCH -n 1
 #SBATCH --partition main,main2
 #SBATCH --mem-per-cpu=4000
