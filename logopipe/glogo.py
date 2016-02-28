0cbad4720458ee48fbbb84094235ba587d5b04e1
William Patterson <wpatt2@pdx.edu>
2016-02-27 20:05:15 -0800

    changed hardcoded strings to variables

--- logopipe/glogo.py
+++ logopipe/glogo.py
@@ -1,15 +1,17 @@
 import os
 
-command = "{exe} --model {mod}/BUPwithMUASimple1.nlogo --experiment {unique} --table /home/wpatt2/CapDoc_out/{unique} --threads 8 > {stdo}{unique}.output"
+command = "{exe} --model {mod}/BUPwithMUASimple1.nlogo --experiment {unique} --table {out}/{unique} --threads 8 > {stdo}/{unique}.output"
 exe = "/share/apps/user/stow/netlogo-5.2.1/netlogo-headless.sh"
-mod = "/home/wpatt2/allExp"
-stdout = "/home/wpatt2/CapDoc_out/stdout/"
+mod = "/home/wpatt2/allExp/"
+out = "/home/wpatt2/NewIns_out/"
+stdout = os.path.join(out, "stdout")
+#command = "({}) &".format(command)
 
 commands = []
-with open("/home/wpatt2/allExp/CapExps.csv", 'r') as cfile:
+with open("/home/wpatt2/allExp/NewIns.csv", 'r') as cfile:
     for line in cfile:
         uni = line.replace("\n", "")
-        tmp_cmd = command.format(exe=exe, unique=uni, mod=mod, stdo=stdout)
+        tmp_cmd = command.format(exe=exe, unique=uni, mod=mod, stdo=stdout, out=out)
         commands.append(tmp_cmd)
 print(len(commands))
 
@@ -31,7 +33,10 @@ print(len(double_commands))
 
 for cabinet in range(0, 3):
     for node in range(0, 10):
-        os.system("(ssh compute-{cab}-{nod} '{cmd}')&".format(cab=cabinet, nod=node, cmd=double_commands.pop(0)))
+        try:
+            os.system("(ssh compute-{cab}-{nod} '{cmd}')&".format(cab=cabinet, nod=node, cmd=commands.pop(0)))
+        except IndexError:
+            pass
         #os.system("ssh compute-{cab}-{nod} 'killall -u wpatt2 /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.65-0.b17.el6_7.x86_64/jre/bin/java'".format(cab=cabinet, nod=node))
         #print("compute-{cab}-{nod}".format(cab=cabinet, nod=node))
         #print("ssh compute-{cab}-{nod} '{cmd}'&".format(cab=cabinet, nod=node, cmd=double_commands.pop(0)))
@@ -40,7 +45,7 @@ print()
 print("++++++++++++++++++++++++++++")
 print()
 
-unrun_cmds = "/home/wpatt2/Alex_out/EXPIREMENTS_NOT_YET_RUN.txt"
+unrun_cmds = os.path.join(stdout, "EXPIREMENTS_NOT_YET_RUN.txt")
 with open(unrun_cmds, 'w') as efile:
     for cmd in double_commands:
         print(cmd)
