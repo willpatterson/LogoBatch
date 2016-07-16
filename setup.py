16504f96a5effab689cc4bdfda5dbea0fb476f94
willpatterson <wpatt2@pdx.edu>
2016-07-16 16:23:22 -0700

    Bumped version down to 0.0.0

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name="logopipe",
-      version="0.0.2",
-      description="Creates, schedules, and pipes output for jobs in slurm. This Release is BETA 1.0",
+      version="0.0.0",
+      description="Creates, schedules, and pipes output for jobs in slurm.",
       license="MIT",
       author="William Patterson",
       packages=find_packages(),
