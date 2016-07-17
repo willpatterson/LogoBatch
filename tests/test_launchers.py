6d44b88cde6bf75de666db7e047decdd18750a14
willpatterson <wpatt2@pdx.edu>
2016-07-16 18:29:41 -0700

    Added test for remote connection

--- tests/test_launchers.py
+++ tests/test_launchers.py
@@ -1,12 +1,22 @@
 import unittest
+import socket
 
 from logobatch.resources import Launcher
 from logobatch.resources import LocalLauncher
 from logobatch.resources import RemoteLauncher
 
 class TestLauncherBase(unittest.TestCase):
-    def test__new__factory(self):
+    def test__new__factory_local(self):
         assert(isinstance(Launcher(None), LocalLauncher))
+
+    def test__new__factory_remote(self):
+        """
+        Cannot test for instance type because class throws an error if it
+        cannot connect to the device specifed by the hostname and I don't
+        want to setup a host with sshkeys just for testing so I test for an
+        error instead
+        """
+        self.assertRaises(socket.gaierror, lambda: Launcher(' '))
 
 if __name__ == '__main__':
     test_classes = (TestLauncherBase)
